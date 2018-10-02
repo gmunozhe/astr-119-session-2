9fedf3c2f6376f41b7ce06fea6cfa3e1fbe44b6e
gmunozhe <43654418+gmunozhe@users.noreply.github.com>
2018-10-02 13:08:46 -0700

    Added scripts to astr-119-session-2

---
+++ operators.py
@@ -0,0 +1,40 @@
+x=9
+y=3	#integers
+
+#arithmetic operators
+print(x+y) #addition
+print(x-y) #subtraction
+print(x*y) #multiplication
+print(x/y) #division
+print(x%y) #modulus, remainder from a division
+print(x**y) #exponentiation
+x = 9.1918123
+print(x//y) #floor division
+
+#Assignment operators
+x = 9 	#sets x equal to 9, changing value
+x += 3 #x = x + 3
+print(x)
+x=9
+x -= 3 #x = x - 3
+print(x)
+
+x *= 3 #x= x * 3
+print(x)
+
+x/= 3	#x= x/3
+print (x)
+
+x **= 3	#x = x**3
+print(x)
+
+#comparison operators
+x=9
+y=3
+
+print(x==y)	#True if x equals y, false otherwise
+print(x!=y)	#True if x does not equal y, false other
+print(x>y)	#True if x is greater than y
+print(x<y)	#True if x is less than y
+print(x>=y)	#True if x great or eq to y
+print(x<=y)
